2065d9044d5e8cff247797131205b211297d2d2d
geethasreebhumaka <geethasreebhumaka10@gmail.com>
2025-12-26 14:06:35 +0530

    leap year or not

--- main (1).py
+++ main (1).py
@@ -1,5 +1,8 @@
-x=int(input())
-for i in range(x):
-    for j in range(x):
-        print("*",end=" ")
-    print()
+year=int(input())
+if year%4==0 and year%100!=0:
+    print("leap year")
+elif year%400==0:
+    print("leap year")
+else:
+    print("not leap year")
+            
